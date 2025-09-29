bb3f9f359971dbf39f46ef903731f8df28602844
Beenz <CH.SABEEN@GMAIL.COM>
2025-09-29 12:18:14 -0700

    added handle functions to App.js

--- src/App.js
+++ src/App.js
@@ -15,14 +15,49 @@ export default function App() {
   const [editid, setEditid] = useState(0);
   const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || "medium");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();    
+
+    if(editid){
+      const date = new Date();
+      const selectedTask = tasklist.find(task => task.id === editid);
+      const updateTask = tasklist.map((e) => (e.id === selectedTask.id ? (e = {id: e.id, name: task, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`}) : {id: e.id, name: e.name, time: e.time}));
+      setTasklist(updateTask);
+      setEditid(0);
+      setTask("");
+      return;
+    }
+
+    if(task){
+      const date = new Date();
+      setTasklist([...tasklist, {id: date.getTime(), name: task, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`}]);
+      setTask("");
+    }
+
+  }
+
+  const handleEdit = (id) => {
+    const selectedTask = tasklist.find(task => task.id === id);
+    setTask(selectedTask.name);
+    setEditid(id);
+  }
+
+  const handleDelete = (id) => {
+    const updatedTasklist = tasklist.filter(task => task.id !== id);
+    setTasklist(updatedTasklist);
+  }
+
   return (
     <div className={'App'+theme}>
        <div className="container"></div>
         <Header setTheme={setTheme} theme={theme}>
           Taskmate
         </Header>
+        <AddTask handleSubmit={handleSubmit} editid={editid} task={task} setTask={setTask}/>
+        <ShowTask tasklist={tasklist} setTasklist={setTasklist} handleEdit={handleEdit} handleDelete={handleDelete}/>
+      </div>
       
-    </div>
+    
   );
 }
 
